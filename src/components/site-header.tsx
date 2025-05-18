faea9be58678650189ee8f9ec3587bd8f514c089
Thomas Meyer <strayfatty@gmail.com>
2025-05-18 15:43:17 +0200

    WIP

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -1,13 +1,13 @@
+import { GitHubLink } from "~/components/ui/github-link";
 import { SidebarTrigger } from "~/components/ui/sidebar";
 import { ThemeToggle } from "~/components/ui/theme-toggle";
-import { GitHubLink } from "~/components/ui/github-link";
 
 export function SiteHeader() {
   return (
     <header className="flex justify-between py-2 pr-6 pl-1">
       <SidebarTrigger />
       <div className="flex gap-4">
-        <GitHubLink project="strayfatty/nextjs-template"/>
+        <GitHubLink project="strayfatty/nextjs-template" />
         <ThemeToggle />
       </div>
     </header>
