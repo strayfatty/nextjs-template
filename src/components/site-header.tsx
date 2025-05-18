593480e3dcf77e1c136b9805734299a3e71aac18
Thomas Meyer <strayfatty@gmail.com>
2025-05-18 15:42:47 +0200

    github button

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -1,21 +1,13 @@
 import { SidebarTrigger } from "~/components/ui/sidebar";
 import { ThemeToggle } from "~/components/ui/theme-toggle";
+import { GitHubLink } from "~/components/ui/github-link";
 
 export function SiteHeader() {
   return (
     <header className="flex justify-between py-2 pr-6 pl-1">
       <SidebarTrigger />
       <div className="flex gap-4">
-        <a
-          href="https://github.com/strayfatty/nextjs-template"
-          target="_blank"
-          className="inline-flex size-9 items-center justify-center rounded-md font-medium text-sm ring-offset-background transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:fill-white"
-          rel="noreferrer"
-        >
-          <svg className="size-6">
-            <use href="/icons/github.svg#github" />
-          </svg>
-        </a>
+        <GitHubLink project="strayfatty/nextjs-template"/>
         <ThemeToggle />
       </div>
     </header>
--- src/components/ui/theme-toggle.tsx
+++ src/components/ui/theme-toggle.tsx
@@ -8,6 +8,7 @@ export function ThemeToggle() {
 
   return (
     <button
+      type="button"
       className="inline-flex size-9 items-center justify-center rounded-md font-medium text-sm ring-offset-background transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50"
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
     >
