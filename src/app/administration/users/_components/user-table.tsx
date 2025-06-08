4833c1e97e82a25210219ca23db209446f3bd32f
Thomas Meyer <strayfatty@gmail.com>
2025-06-08 12:58:00 +0200

    shadcn table

--- src/app/administration/users/_components/user-table.tsx
+++ src/app/administration/users/_components/user-table.tsx
@@ -3,6 +3,8 @@
 import { usePagination } from "~/hooks/use-pagination";
 import { api } from "~/trpc/react";
 import { userTableDefaultQuery } from "./user-table-default-query";
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "~/components/ui/table";
+import { Button } from "~/components/ui/button";
 
 export function UserTable() {
   const { offset, limit, setPagination, pagination } = usePagination(
@@ -17,27 +19,34 @@ export function UserTable() {
 
   return (
     <div>
-      {users?.map((u) => (
-        <div key={u.id}>
-          {u.name}, {u.id}, {u.email}, {u.image}
-        </div>
-      ))}
-      <button
-        type="button"
-        onClick={() =>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Name</TableHead>
+              <TableHead>Email</TableHead>
+              <TableHead>Image</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {users?.map(user => (
+              <TableRow key={user.id}>
+                <TableCell>{user.name}</TableCell>
+                <TableCell>{user.email}</TableCell>
+                <TableCell>{user.image}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+      <div>
+        <Button variant="outline" size="sm" onClick={() =>
           setPagination({ pageIndex: pageIndex - 1, pageSize: pageSize })
-        }
-      >
-        prev
-      </button>
-      <button
-        type="button"
-        onClick={() =>
+        } disabled={false}>prev</Button>
+        <Button variant="outline" size="sm" onClick={() =>
           setPagination({ pageIndex: pageIndex + 1, pageSize: pageSize })
-        }
-      >
-        next
-      </button>
+        } disabled={false}>next</Button>
+      </div>
     </div>
   );
 }
